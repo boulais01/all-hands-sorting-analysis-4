54389583e9bae7cca00906e0445ed03fd696acca
Simon Jones <github@simonjjones.com>
2024-03-29 09:45:09 -0400

    feat: removing unecessary enumerations

--- de/de/enumerations.py
+++ de/de/enumerations.py
@@ -1,46 +1,12 @@
-"""Configuration of the benchmarking tool and its approaches."""
+"""Configuration of the doubling experiment tool."""
 
 from enum import Enum
-
-
-class ListType(str, Enum):
-    """Define the type of the lists subject to benchmarking."""
-
-    singlylinked = "singlylinked"
-    doublylinked = "doublylinked"
-
-    def __str__(self):
-        """Define a default string representation."""
-        return self.value
 
 
 class ListData(str, Enum):
     """Define the data that will be stored in the list nodes of the lists."""
 
     ints = "ints"
-
-    def __str__(self):
-        """Define a default string representation."""
-        return self.value
-
-
-class BenchmarkStrategy(str, Enum):
-    """Define the benchmarking strategy."""
-
-    double = "double"
-
-    def __str__(self):
-        """Define a default string representation."""
-        return self.value
-
-
-class BenchmarkOperation(str, Enum):
-    """Define the benchmarking operation."""
-
-    removefirst = "removefirst"
-    removelast = "removelast"
-    add = "add"
-    iadd = "iadd"
 
     def __str__(self):
         """Define a default string representation."""
