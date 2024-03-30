21303b7fc74636f61e2e1b101bb40deafc2eb040
Simon Jones <github@simonjjones.com>
2024-03-30 18:14:10 -0400

    fix: generate.py: adding check to ensure lengths are doubled

--- de/tests/test_generate.py
+++ de/tests/test_generate.py
@@ -11,9 +11,15 @@ def test_generate_lists_zero():
 
 def test_generate_lists():
     """Ensure the function does not fail with basic input."""
-    assert len(generate.generate_lists(enumerations.ListData.ints, 1, 5)) == 5
-    assert len(generate.generate_lists(enumerations.ListData.floats, 1, 5)) == 5
-    assert len(generate.generate_lists(enumerations.ListData.strings, 1, 5)) == 5
+    generated_ints = generate.generate_lists(enumerations.ListData.ints, 1, 5)
+    assert len(generated_ints) == 5
+    assert [len(the_list) for the_list in generated_ints] == [1 * (2 ** i) for i in range(5)]
+    generated_floats = generate.generate_lists(enumerations.ListData.floats, 1, 5)
+    assert len(generated_floats) == 5
+    assert [len(the_list) for the_list in generated_floats] == [1 * (2 ** i) for i in range(5)]
+    generated_strings = generate.generate_lists(enumerations.ListData.strings, 1, 5)
+    assert len(generated_strings) == 5
+    assert [len(the_list) for the_list in generated_strings] == [1 * (2 ** i) for i in range(5)]
 
 def test_generate_list_with_ints_zero():
     """Ensure the function can handle an input of zero."""
