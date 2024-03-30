d0520b46bec6dc0bda9ac820327373ed125f69e1
Rebekah Rudd <rudd01@allegheny.edu>
2024-03-30 13:21:31 -0400

    added function to determine parameter types

--- de/de/path.py
+++ de/de/path.py
@@ -1,13 +1,34 @@
 """Functions that extract the filename and function name."""
 # using getattr()
 
+import inspect
+import simon's generator module
 
-def path(filename):
-    import filename 
+# how to get the parameters - is that somethign that I need to do
+def path(filename: path, funcname: string) -> ??:
+    import filename
+    if filename.hasattr() is False:
+        print("This file does not seem to have that function.")
     try:
         filename.getattr(sort, )
     catch:
         print("Are you sure this is a sorting algorithm?")
+    
+    parameters = get_parameters(filename, funcname)
+    if parameters == type int:
+        call int generator function
+    if parameters == type str:
+        call str generator function
+    if parameters == type float:
+        call float generator function
 
+# https://stackoverflow.com/questions/23228664/how-to-check-which-arguments-a-function-method-takes
+# hasattr() - checks if a file has the function
 
 # def function(funcname):
+
+def get_parameters(filename, funcname):
+    import filename
+    function_call = filename.funcname()
+    parameters = inspect.getargspec(function_call).args
+    return parameters
