8d39bfd56abe08c0f4e622c098dadfbd742eb230
boulais01 <boulais01@DESKTOP-57IM70B>
2024-04-01 21:28:53 -0500

    feat: begin rough draft

--- de/de/benchmark.py
+++ de/de/benchmark.py
@@ -1,69 +1,62 @@
 """Run a benchmark on a function provided by a list-based structure."""
 
+import time
+from typing import List, Tuple
+
 from de import enumerations, generate
-from typing import Tuple, List
-from copy import deepcopy
-import timeit
+from de.constants import constants
 
 # ruff: noqa: PLR0913
 
-# FIXME: modify this to meet the needed project functionality
-
 def benchmark(
-    listtype: enumerations.ListType,
     listdata: enumerations.ListData,
-    strategy: enumerations.BenchmarkStrategy,
-    operation: enumerations.BenchmarkOperation,
+    functype: enumerations.FunctionParamTypes,
     startsize: int,
-    runs: int
+    runs: int,
 ) -> List[Tuple[int, int, float]]:
-    """Benchmark the given operation on the given list."""
-    benchmark_data: List[Tuple[int, int, float]] = []
-    lists_to_use = generate.generate_lists_with_strategy(listtype, listdata, strategy, runs, startsize)
-    run_index = 1
-    for list_to_use in lists_to_use:
-        list_to_use_copy = deepcopy(list_to_use)
-        list_to_use_size = len(list_to_use)
-        if operation == enumerations.BenchmarkOperation.removefirst:
-            input("about to create removefirst...")
-            print(list_to_use.display())
-            print(len(list_to_use))
-            lambda_func = lambda: list_to_use.removefirst()
-        elif operation == enumerations.BenchmarkOperation.removelast:
-            lambda_func = lambda: list_to_use.removelast()
-        elif operation == enumerations.BenchmarkOperation.add:
-            lambda_func = lambda: list_to_use + list_to_use
-        elif operation == enumerations.BenchmarkOperation.iadd:
-            lambda_func = lambda: list_to_use.__iadd__(list_to_use_copy)
-
-        # test the lambda_func 5 times
-        delta_t = timeit.timeit(lambda_func, number=5) / 5
-
-        benchmark_data.append((run_index, list_to_use_size, delta_t))
-        run_index += 1
-    return benchmark_data
+    """Benchmark the data."""
+    # initialize list and size, setup list generation
+    times_list = []
+    size = startsize
+    generate_func = getattr(generate, "generate_list_with_" + listdata)
+    list_one = generate_func(listtype, size)
+    # return list of tuples (run_num, size, run_time)
+    return times_list
 
 
-def find_minimum(benchmark_data: List[Tuple[int, int, float]]) -> Tuple[int, int, float]:
-    """Find the minimum of the provided benchmark data."""
-    return min(benchmark_data, key=lambda x: x[2])
+def find_minimum(
+    times_list: List[Tuple[int, int, float]],
+) -> List[Tuple[int, int, float]]:
+    """Find quickest run in list."""
+    # return all data for a row in a tuple
+    min_time = times_list[0]
+    for i in times_list:
+        if (
+            i[2]
+            < min_time[2]
+        ):
+            min_time = i
+    return min_time
 
 
-def find_maximum(benchmark_data: List[Tuple[int, int, float]]) -> Tuple[int, int, float]:
-    """Find the maximum of the provided benchmark data."""
-    return max(benchmark_data, key=lambda x: x[2])
+def find_maximum(
+    times_list: List[Tuple[int, int, float]],
+) -> List[Tuple[int, int, float]]:
+    """Find slowest run in list."""
+    # return all data for a row in a tuple
+    max_time = times_list[0]
+    for i in times_list:
+        if (
+            i[2]
+            > max_time[2]
+        ):
+            max_time = i
+    return max_time
 
 
-def find_average(benchmark_data: List[Tuple[int, int, float]]) -> float:
-    """Find the average of the provided benchmark data."""
-    delta_ts = map(lambda x: x[2], benchmark_data)
-    return sum(delta_ts) / len(delta_ts)
-
-
-def find_doubling_ratios(benchmark_data: List[Tuple[int, int, float]]) -> List[float]:
-    """Return a list of doubling ratios for a sequence of runs."""
-    doubling_ratios: List[float] = []
-    for i in range(len(benchmark_data) - 1):
-        # append the ratio of their delta_ts
-        doubling_ratios.append(benchmark_data[i + 1][2] / benchmark_data[i][2])
-    return doubling_ratios
+def compute_average(times_list: List[Tuple[int, int, float]]) -> float:
+    """Compute average runtime."""
+    list_sum = 0
+    for i in times_list:
+        list_sum += i[2]
+    return list_sum / len(times_list)
