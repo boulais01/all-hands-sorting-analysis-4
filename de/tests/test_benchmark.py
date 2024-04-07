656c5706b7286d184a763672a2e4d69e4a547eff
boulais01 <boulais01@DESKTOP-57IM70B>
2024-04-07 11:16:06 -0500

    feat: benchmark test base

---
+++ de/tests/test_benchmark.py
@@ -0,0 +1,13 @@
+"""Tests for the benchmark module."""
+
+from de import benchmark, enumerations
+
+def test_benchmark_ints():
+    """Tests that the benchmark functions works as expected with integer input."""
+    benchmark(listdata: enumerations.ListData,functype: enumerations.FunctionParamTypes,funcname: Callable,startsize: int,runs: int,)
+
+def test_find_minimum():
+
+def test_find_maximum():
+
+def test_compute_average():
